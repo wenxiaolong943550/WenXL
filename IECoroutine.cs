77ce934fa805d34f647df9de3c8c8beddf99e0ad
WxlGit <943550848@qq.com>
2021-10-26 22:47:30 +0800

    添加协程管理器

---
+++ IECoroutine.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using System;
+
+public static class IECoroutine
+{
+    private static IEnumeratorMonoBehaviour _IEnumeratorMonoBehaviour = null;
+
+    public static Coroutine Run(IEnumerator ie)
+    {
+        if (_IEnumeratorMonoBehaviour == null)
+        {
+            GameObject obj = new GameObject("_IEnumeratorMonoBehaviour");
+            if (Application.isPlaying)
+                GameObject.DontDestroyOnLoad(obj);
+            _IEnumeratorMonoBehaviour = obj.AddComponent<IEnumeratorMonoBehaviour>();
+        }
+        return _IEnumeratorMonoBehaviour.StartCoroutine(ie);
+    }
+
+    public static void Run(float time, Action callBack)
+    {
+        Run(WaitingToDo(time, callBack));
+    }
+
+    public static void Stop(Coroutine con)
+    {
+        if (con != null && _IEnumeratorMonoBehaviour != null)
+        {
+            _IEnumeratorMonoBehaviour.StopCoroutine(con);
+        }
+    }
+
+    static IEnumerator WaitingToDo(float time, Action callBack)
+    {
+        yield return new WaitForSeconds(time);
+        callBack?.Invoke();
+    }
+}
+
+public class IEnumeratorMonoBehaviour : MonoBehaviour
+{
+
+}
